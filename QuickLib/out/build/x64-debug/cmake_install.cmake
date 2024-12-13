bafa239905254e8bfe74e159525f1906482f0112
Alex S <Alexander2.Silvester@live.uwe.ac.uk>
2024-12-13 15:06:10 +0000

    idk made some changes

--- QuickLib/out/build/x64-debug/cmake_install.cmake
+++ QuickLib/out/build/x64-debug/cmake_install.cmake
@@ -32,12 +32,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/alexs/various cpp/QuickLib/QuickLib/out/build/x64-debug/_deps/sfml-build/cmake_install.cmake")
-
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
